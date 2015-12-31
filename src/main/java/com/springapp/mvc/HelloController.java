c8f6dab296f988724168a3e84a413aae5fc838c8
zj <zj@xiaoxiao.la>
2015-12-31 11:38:10 +0800

    dd

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -15,36 +15,9 @@ import java.util.Map;
 	@RequestMapping(method = RequestMethod.GET)
 	@ResponseBody
 	public Map<String,Object>  printWelcome() {
-//		model.addAttribute("message", "well done!");
-
-		Map<String,Object> result =new HashMap<String,Object>();
+        Map<String,Object> result =new HashMap<String,Object>();
 		result.put("zj","something");
-        result.put("s",new Person("aaa",1));
 		return result;
 	}
- class Person{
-  private  String aaa;
-  private  Integer age;
 
-  public Person(String aaa, Integer age) {
-   this.aaa = aaa;
-   this.age = age;
-  }
-
-  public String getAaa() {
-   return aaa;
-  }
-
-  public void setAaa(String aaa) {
-   this.aaa = aaa;
-  }
-
-  public Integer getAge() {
-   return age;
-  }
-
-  public void setAge(Integer age) {
-   this.age = age;
-  }
- }
 }
