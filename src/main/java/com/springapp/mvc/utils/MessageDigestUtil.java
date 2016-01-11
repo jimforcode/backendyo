84d0f9b1fb4141847bffbc7fbc0a38a4672253d1
zj <zj@xiaoxiao.la>
2016-01-11 10:56:56 +0800

    Crypy

--- src/main/java/com/springapp/mvc/utils/MessageDigestUtil.java
+++ src/main/java/com/springapp/mvc/utils/MessageDigestUtil.java
@@ -14,34 +14,25 @@ public class MessageDigestUtil {
     public static String encrypMD5(byte[] data) throws NoSuchAlgorithmException {
 
             MessageDigest md5 = MessageDigest.getInstance("MD5");
-//        byte[] result= md5.digest(data);
-        md5.update(data);
+         md5.update(data);
 
-//        return  Hex.encodeHexString(result);
-        return  Hex.encodeHexString(md5.digest());
+         return  Hex.encodeHexString(md5.digest());
     }
 
     public static String encrypSHA1(byte[] data) throws NoSuchAlgorithmException {
 
         MessageDigest sha = MessageDigest.getInstance("SHA");
-//        byte[] result= sha.digest(data);
-                sha.update(data);
+                 sha.update(data);
         return  Hex.encodeHexString(sha.digest());
     }
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
-        System.out.println(encrypMD5("zengjin".getBytes()));
-        System.out.println(encrypMD5("zengjin".getBytes()).equals(encrypMD5("zengjin".getBytes())));
-        System.out.println(encrypMD5("zengjin".getBytes()));
-        System.out.println(encrypMD5("zengjin".getBytes()));
-        System.out.println(Crypt.crypt("zengjin"));
-        System.out.println(Crypt.crypt("zengjin"));
-        System.out.println(Crypt.crypt("zengjin"));
-        System.out.println(Crypt.crypt("zengjin"));
-        System.out.println(encrypSHA1("zengjin".getBytes()));
-        System.out.println(encrypSHA1("zengjin".getBytes()));
-        System.out.println(encrypSHA1("zengjin".getBytes()));
-        System.out.println(encrypSHA1("zengjin".getBytes()));
+        String password ="zengjin";
+        String stored= Crypt.crypt(password);
+        System.out.println("stored ==>>>"+stored);
+        System.out.println("Crypt.crypt(password,stored) ==>>>"+Crypt.crypt(password,stored));
+        System.out.print(stored.equals(Crypt.crypt(password,stored)));
+
 
 
     }
